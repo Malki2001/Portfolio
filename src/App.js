0c7c5dc23e56212fca7f05cbf9a82147cde9d31f
Malki2001 <malkiyasodhara18@gmail.com>
2024-08-30 23:29:30 +0530

    updated files

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/NavBar';
+import Banner from './components/Banner';
+import Skills from './components/Skills';
+import AboutMe from './components/AboutMe';
+import MyServices from './components/MyServices';
+import Projects from './components/Projects';
+import Education from './components/Education/Education'
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+       <NavBar/>
+       <Banner/>
+       <AboutMe/>
+       <MyServices/>
+       <Skills/>
+       
+       <Projects/>
+       <Education/>
+       <Contact/>
+       <Footer/>
+    </div>
+  );
+}
+
+export default App;
